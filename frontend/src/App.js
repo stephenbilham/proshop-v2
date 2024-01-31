414ae6bfb223266a112f9775bb63b9d1643996b4
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-01-31 14:04:04 -0800

    git hell

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+const App = () => {
+	return (
+		<div>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<Outlet />
+				</Container>
+			</main>
+			<Footer />
+		</div>
+	);
+};
+
+export default App;
