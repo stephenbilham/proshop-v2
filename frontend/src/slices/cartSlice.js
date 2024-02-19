e066524b963d39637b0b9c97501ea1661e37dc77
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-02-19 13:33:04 -0800

    payments screen

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -36,10 +36,18 @@ const cartSlice = createSlice({
 			state.shippingAddress = action.payload;
 			updateCart(state);
 		},
+		savePaymentMethod: (state, action) => {
+			state.paymentMethod = action.payload;
+			updateCart(state);
+		},
 	},
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress } =
-	cartSlice.actions;
+export const {
+	addToCart,
+	removeFromCart,
+	saveShippingAddress,
+	savePaymentMethod,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
