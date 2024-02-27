f468002af58d9d459b995b0fb3e4dd237f3b766a
stephen-bilham-tm <stephenbilham@yahoo.com>
2024-02-27 09:49:22 -0800

    add loader back

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import { Carousel, Image } from "react-bootstrap";
+import Loader from "./Loader";
 import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
 const ProductCarousel = () => {
-	const { data: products, error } = useGetTopProductsQuery();
+	const { data: products, isLoading, error } = useGetTopProductsQuery();
 
-	if (error) {
-		return <Message variant="danger">{error}</Message>;
-	}
-
-	return (
+	return isLoading ? (
+		<Loader />
+	) : error ? (
+		<div>
+			<Message variant="danger">{error?.data?.message || error.error}</Message>
+		</div>
+	) : (
 		<Carousel pause="hover" className="bg-primary mb-4">
 			{products.map((product) => (
 				<Carousel.Item key={product._id}>
